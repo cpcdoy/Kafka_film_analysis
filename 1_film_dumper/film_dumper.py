7ff5788648f661bc125f51ff15d9ae44caf0b507
Chady Dimachkie <dimach_c@epita.fr>
2017-07-05 21:06:51 +0200

    Add: now sends the JSON in real-time to the Kafka brokers

--- 1_film_dumper/film_dumper.py
+++ 1_film_dumper/film_dumper.py
@@ -1,13 +1,16 @@
 import tmdbsimple as tmdb
 import numpy as np
 from kafka import KafkaProducer
-from kafka.errors import KafkaError
-import json, sys, getopt
+import json, sys
 
-#producer = KafkaProducer(bootstrap_servers=['192.168.0.30:9092'])
+class Review(json.JSONEncoder):
+    def __init__(self, author, id, content):
+        self.author = author
+        self.id = id
+        self.content = content
 
 class Movie(json.JSONEncoder):
-    def __init__(self, id, title, release_date, popularity, revenue, budget, genre_list, review_list):
+    def __init__(self, id, title, release_date, popularity, revenue, budget, genre_list, review_list, analyzed_popularity):
         self.id = id
         self.title = title
         self.release_date = release_date
@@ -16,8 +19,7 @@ class Movie(json.JSONEncoder):
         self.budget = budget
         self.genre_list = genre_list
         self.review_list = review_list
-    def __init__(self, json_dict_string):
-        self.__dict__ = json_dict_string
+        self.analyzed_popularity = analyzed_popularity
 
 def dumper(nb_movies):
     tmdb.API_KEY = '6130738744e761b316bcdb464c23e5ea'
@@ -25,16 +27,20 @@ def dumper(nb_movies):
     movie_list = []
     review_list = []
     genre_list = []
-    nb_of_dumped_movies = 0
-    i = 10
-    while nb_of_dumped_movies < nb_movies:
+    nb_dumped_movies = 0
+    i = 0
+
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+
+    while nb_movies > nb_dumped_movies:
         movie = tmdb.Movies(i)
         try:
             reponse = movie.info()
             reviews = movie.reviews()
             if (len(reviews["results"])):
-                nb_of_dumped_movies += 1
-                print(nb_of_dumped_movies)
+                print('Found review: ', i)
+                nb_dumped_movies += 1
+                print(nb_dumped_movies)
                 genre_list = []
                 for g in movie.genres:
                     genre_list.append(g["name"])
@@ -42,7 +48,8 @@ def dumper(nb_movies):
                 for r in reviews["results"]:
                     review_list.append(r["content"])
 
-                movie_list.append(Movie(i, movie.title, movie.release_date, movie.popularity, movie.revenue, movie.budget, genre_list, review_list))
+                json_str_dump = json.dumps(Movie(i, movie.title, movie.release_date, movie.popularity, movie.revenue, movie.budget, genre_list,  review_list, -1).__dict__)
+                producer.send('test', key=b'film', value=json_str_dump.encode('ascii'))
 
                 genre_list = []
                 review_list = []
@@ -50,28 +57,24 @@ def dumper(nb_movies):
             pass
         i += 1
     json_string = json.dumps([m.__dict__ for m in movie_list])
-    data = json.loads(json_string)
-    for m in data:
-        str_as_bytes = str.encode(m["title"])
-        print(str_as_bytes)
     return json_string
 
-def dump_and_write_movies_to_json(nb_movies):
+def write_movies_to_jsonfile(path, nb_movies):
     json_string = dumper(nb_movies)
-    with open('data/movie_data.json', 'w') as outfile:
+    with open(path, 'r+') as outfile:
         json.dump(json_string, outfile, sort_keys = True, indent = 4, ensure_ascii = False)
 
-def load_movies_from_jsonfile(path):
-    data = json.loads(open(path).read())
-    jsonmovies = json.loads(data)
-    for m in jsonmovies:
-        print(m["title"])
-    return jsonmovies
-
 def main():
-    if len(sys.argv) > 1:
-        if sys.argv[1].isdigit():
-            dump_and_write_movies_to_json(int(sys.argv[1]))
+    if (len(sys.argv) > 1):
+        if (sys.argv[1].isdigit()):
+            write_movies_to_jsonfile('data/movie_data.json', int(sys.argv[1]))
+    return
 
 if __name__ == "__main__":
     main()
+
+
+
+#data = json.loads(open('data/movie_data.json').read())
+#Json list of movies
+#jsonmovies = json.loads(data)
